eddb11e18fdc846b10ace3ae14112aa1ed568fec
Sumit <sumitsunchu@gmail.com>
2024-09-18 19:57:50 -0700

    Trying to implement Database

---
+++ app/Database/user.js
@@ -0,0 +1,7 @@
+export default function User(email, familyName, givenName, name, id) {
+    this.email = email;
+    this.familyName = familyName;
+    this.givenName = givenName;
+    this.name = name;
+    this.id = id;
+  }
